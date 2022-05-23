178f246bd2c0c9d92e16b8b46668ea266e3ff94a
Luciana0811 <77796175+Luciana0811@users.noreply.github.com>
2022-05-22 23:49:58 -0500

    Add files via upload

---
+++ LAB08_GRUPO_A_ HUAMAN _COAQUIRA_LUCIANA/EJERCICIO 1/Rectangulo.h
@@ -0,0 +1,22 @@
+#ifndef RECTANGULO_H
+#define RECTANGULO_H
+
+#include <iostream>
+
+using namespace std;
+#include "Figura.h"
+
+class Rectangulo : public Figura
+{
+private:
+	float L_menor, L_mayor;
+public:
+	Rectangulo(string,int,string,float,float);
+	~Rectangulo();
+	void imprimir();
+	void area();
+	void perimetro();
+	void cambioTamanio();
+};
+#endif
+
---
+++ LAB08_GRUPO_A_ HUAMAN _COAQUIRA_LUCIANA/EJERCICIO 1/main.cpp
@@ -0,0 +1,40 @@
+#include "Rectangulo.h"
+#include "Figura.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+	int coordenadas[2]= {0,0};
+	string opcion;
+	Rectangulo fig = Rectangulo("Azul",coordenadas[2],"Rectangular",12,23);
+	while(true){
+	cout<<"---------INDICAR LA OPCION QUE DESEA OBSERVAR---------\n"<<endl;
+	cout<<"1. CAMBIAR COLOR "<<endl;
+	cout<<"2. CAMBIAR TAMANO "<<endl;
+	cout<<"3. MOVER UBICACION DE COORDENADA (X,Y)=(0,0) "<<endl;
+	cout<<"4. MOSTRAR DATOS DE LA FIGURA "<<endl;
+	cout<<"5. SALIR " <<endl;
+	cin>>opcion;
+		if (opcion=="1"){
+			fig.cambiarColor();
+		}
+		else if (opcion=="2"){
+			fig.cambioTamanio();
+		}
+		else if (opcion=="3"){
+			fig.moverCoordenadas();
+		}
+		else if (opcion=="4"){
+			fig.imprimir();
+			fig.obtenerColor();
+			fig.perimetro();
+		}
+		else if (opcion=="5"){
+			break;
+		}
+		else{
+			cout<<"OPCION NO VALIDA"<<endl;
+		}
+	}
+	return 0;
+}
