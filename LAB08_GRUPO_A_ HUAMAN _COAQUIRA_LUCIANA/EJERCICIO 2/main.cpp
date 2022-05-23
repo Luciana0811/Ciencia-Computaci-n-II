b66d8277b93429a98731bbe58c97c3df6d4c6e60
Luciana0811 <77796175+Luciana0811@users.noreply.github.com>
2022-05-22 23:50:24 -0500

    Add files via upload

---
+++ LAB08_GRUPO_A_ HUAMAN _COAQUIRA_LUCIANA/EJERCICIO 2/main.cpp
@@ -0,0 +1,42 @@
+#include "Rectangulo.h"
+#include "Figura.h"
+#include "Elipse.h"
+#include "Rectangulo.h"
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	int coordenadas[2]= {0,0};
+	string opcion;
+	while(true){
+		cout<<"---------INDICAR LA FIGURA---------\n"<<endl;
+		cout<<"1. RECTANGULO "<<endl;
+		cout<<"2. ELIPSE "<<endl;
+		cout<<"3. SALIR " <<endl;
+		cin>>opcion;
+		if (opcion=="1"){
+			Rectangulo fig = Rectangulo("VERDE",coordenadas[2],"Rectangular",10,13);
+			fig.imprimir();
+			fig.area();
+			fig.perimetro();
+			fig.cambioTamanio();
+
+		}
+		else if (opcion=="2"){
+			Elipse fig1 = Elipse("Amarillo",coordenadas[2],"Elipse",5,12);
+			fig1.imprimir();
+			
+		}
+		else if (opcion=="3"){
+			break;
+		}
+		else{
+			cout<<"OPCION NO VALIDA"<<endl;
+		}
+	}
+	return 0;
+}
+	
+
