0e293314e178fac4c47cc0018965f98b62651b39
Luciana0811 <77796175+Luciana0811@users.noreply.github.com>
2022-06-12 02:54:50 -0500

    Add files via upload

---
+++ LAB10_GRUPO_A_ HUAMAN _COAQUIRA_LUCIANA/EJERCICIO 3/list.h
@@ -0,0 +1,33 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+#include "node.h"
+#include "node.cpp"
+
+using namespace std;
+
+template <class T>
+
+class Lista
+{
+public:
+	Lista();
+	~Lista();
+	void agregar_al_inicio(T);
+	void agregar_al_final(T);
+	void insertar(T);
+	void imprimir();
+	void elementos_random(int);
+
+	
+private:
+	Nodo<T> *m_head;
+	int m_num_nodes;
+};
+
+#endif // LIST_H
---
+++ LAB10_GRUPO_A_ HUAMAN _COAQUIRA_LUCIANA/EJERCICIO 3/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+
+
+#include "list.h"
+#include "list.cpp"
+
+using namespace std;
+
+int main()
+{
+	Lista<int> lista_1;
+	Lista<int> lista_2;
+	int elemento;
+	int posicion;
+	int op;
+	int dimension;
+
+	
+	cout << "INDICAR EL TAMANIO DE LA LISTA: " << endl;
+	cin >> dimension;
+	
+	lista_1.elementos_random(dimension);
+	
+	cout << "LISTA INICIAL: " << endl;
+	lista_1.imprimir();
+	while(true){
+		cout<<"MENU: \n 1.-AGREGAR ELEMENTO \n 2.-SALIR"<<endl;
+		cin>>op;
+		if(op==1){
+			cout << "AGREGAR ELEMENTO QUE SERA INSERTADO ORDENADAMENTE: " << endl;
+			cin >> elemento;
+			cout<<"INDICAR LA POSICION EN LA QUE DESEA COLOCAR EL ELEMENTO" <<endl;
+			cin >> posicion;
+			lista_1.insertar(elemento);
+			lista_1.imprimir();
+		}
+		else{
+			break;
+		}
+	}
+
+	return 0;
+	
+}
