a6403329ebf40e61f0d67286f840d558a8d45d55
Luciana0811 <77796175+Luciana0811@users.noreply.github.com>
2022-06-12 03:22:07 -0500

    Add files via upload

---
+++ LAB10_GRUPO_A_ HUAMAN _COAQUIRA_LUCIANA/EJERCICIO 4/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+
+
+#include "list.h"
+#include "list.cpp"
+
+using namespace std;
+
+int main()
+{
+	Lista<int> lista_1;
+	Lista<int> lista_2;
+	int elemento;
+	int pos;
+	int op;
+	int dimension;
+
+	
+	cout << "INDICAR EL TAMANIO DE LA LISTA: " << endl;
+	cin >> dimension;
+	
+	lista_1.elementos_random(dimension);
+	cout<<"LISTA IMPRIMIDA"<<endl;
+	lista_1.imprimir();
+	cout << "INDICAR LA POSICION DEL ELEMENTO QUE DESEA ELIMINAR: " << endl;
+	cin>>op;
+	lista_1.eliminar_nodo(op);
+	lista_1.imprimir();
+
+	return 0;
+	
+}
